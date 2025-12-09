a217807120dc4061378f10dfee877c0788054771
benj-lazaro <benjie.work@gmail.com>
2025-12-09 19:46:34 +0800

    10 how react binds content update

--- 01-welcome-hello-react/10-how-react-binds/code/taskopedia/src/main.jsx
+++ 01-welcome-hello-react/10-how-react-binds/code/taskopedia/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+// Render content on selected HTML element
 createRoot(document.getElementById("root")).render(
   <StrictMode>Hello</StrictMode>
 );
